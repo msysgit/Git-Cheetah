89383fe8b5a14f953f5b7fb0279355680eee120a
Johannes Schindelin <johannes.schindelin@gmx.de>
2007-12-18 20:34:11 +0100

    Add a simple script to kill and restart explorer and task manager
    The hassle with Explorer extensions is that once installed, the explorer
    will not release the DLLs easily.  The script kill-explorer.sh provides
    a quick and dirty way to help that, by killing and restarting explorer.exe
    and taskmgr.exe.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ kill-explorer.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ps -W | grep -e \\\\explorer.exe$ -e \\\\taskmgr.exe$ > /tmp/killtasks.txt
+
+echo
+cat /tmp/killtasks.txt
+echo
+echo "Okay to kill?  If not, ^C"
+read answer
+
+pids=$(cat /tmp/killtasks.txt | sed -n "s/^[^0-9]*\([0-9][0-9]*\).*$/\1/p")
+for p in $pids
+do
+	echo taskkill //f //pid $p
+	taskkill //f //pid $p
+done
+
+/c/WINDOWS/explorer &
+sleep 2
+/c/WINDOWS/system32/taskmgr &
