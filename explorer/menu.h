554697fa6c8294e4555d9d76ef1331dff82ff50a
Erik Faye-Lund <kusmabite@gmail.com>
2010-07-14 22:46:32 +0200

    explorer/menu.h: fixup indentation
    Signed-off-by: Erik Faye-Lund <kusmabite@gmail.com>

--- explorer/menu.h
+++ explorer/menu.h
@@ -3,12 +3,12 @@
 
 extern struct git_menu_virtual_table
 {
-        STDMETHOD(query_interface)(void *, REFIID, PVOID*);
-        STDMETHOD_(ULONG, add_ref)(void *);
-        STDMETHOD_(ULONG, release)(void *);
+	STDMETHOD(query_interface)(void *, REFIID, PVOID*);
+	STDMETHOD_(ULONG, add_ref)(void *);
+	STDMETHOD_(ULONG, release)(void *);
 	STDMETHOD(query_context_menu)(void *,
 			HMENU, UINT, UINT, UINT, UINT);
-        STDMETHOD(invoke_command)(void *, LPCMINVOKECOMMANDINFO);
+	STDMETHOD(invoke_command)(void *, LPCMINVOKECOMMANDINFO);
 	STDMETHOD(get_command_string)(void *,
 			UINT, UINT, PUINT, LPSTR, UINT);
 } git_menu_virtual_table;
